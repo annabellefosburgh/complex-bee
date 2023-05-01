3e7fa9dd24c3ad17408b160d976180842cbfe96f
annabellefosburgh <afosburgh20@gmail.com>
2023-05-01 13:19:29 -0500

    Added inquirer prompts for data input

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,43 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    prompt ([
+        {type: 'input',
+         message: 'What is the title of your web application?',
+         name: 'Title'
+        },
+        {type: 'input',
+         message: 'Write a short description on your project and what problem it solves.',
+         name: 'Description'
+        },
+        {type: 'input',
+         message: 'Please paste a link to your deployed application.',
+         name: 'appLink'
+        },
+        {type: 'input',
+         message: 'Please paste a link to your github repository.',
+         name: 'githubLink'
+        },
+        {type: 'input',
+         message: 'Provide us with some features that are displayed in your application.',
+         name: 'Features'
+        },
+        {type: 'input',
+         message: 'Please describe directions to use your application properly.',
+         name: 'Usage'
+        },
+        {type: 'input',
+         message: 'Include any contributors in your project. If there are none, please include "N/A".',
+         name: 'Contributors'
+        },
+        {type: 'input',
+         message: 'Please include all sources used to make this project.',
+         name: 'Sources'
+        },
+    ])
+const questions = [Title, Description, appLink, githubLink, Features, Usage, Contributors, Sources];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
